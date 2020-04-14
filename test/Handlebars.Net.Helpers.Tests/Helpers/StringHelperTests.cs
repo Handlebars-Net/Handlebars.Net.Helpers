6b51c1edc8159e84dd31a31862a4e95a338bdfa5
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-04-14 12:33:54 +0000

    StringHelperTests

--- test/Handlebars.Net.Helpers.Tests/Helpers/StringHelperTests.cs
+++ test/Handlebars.Net.Helpers.Tests/Helpers/StringHelperTests.cs
@@ -53,6 +53,19 @@ namespace Handlebars.Net.Helpers.Tests.Helpers
         }
 
         [Theory]
+        [InlineData("foo", null, "foo")]
+        [InlineData("foo", "", "foo")]
+        [InlineData("foo", "bar", "barfoo")]
+        public void Prepend(string value, string prepend, string expected)
+        {
+            // Act
+            var result = _sut.Prepend(value, prepend);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
         [InlineData(null, null)]
         [InlineData("", "")]
         [InlineData("Bar", "raB")]
@@ -66,13 +79,116 @@ namespace Handlebars.Net.Helpers.Tests.Helpers
         }
 
         [Theory]
-        [InlineData("foo", null, "foo")]
-        [InlineData("foo", "", "foo")]
-        [InlineData("foo", "bar", "barfoo")]
-        public void Prepend(string value, string prepend, string expected)
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        [InlineData("a", "a")]
+        [InlineData("A", "A")]
+        [InlineData("foo bar", "fooBar")]
+        [InlineData("FOO Bar", "fooBar")]
+        public void ToCamelCase(string value, string expected)
         {
             // Act
-            var result = _sut.Prepend(value, prepend);
+            var result = _sut.ToCamelCase(value);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        [InlineData("a", "a")]
+        [InlineData("A", "a")]
+        [InlineData("foo bar", "foo bar")]
+        [InlineData("FOO Bar", "foo bar")]
+        public void ToLower(string value, string expected)
+        {
+            // Act
+            var result = _sut.ToLower(value);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        [InlineData("a", "A")]
+        [InlineData("A", "A")]
+        [InlineData("foo bar", "FooBar")]
+        [InlineData("FOO Bar", "FOOBar")]
+        public void ToPascalCase(string value, string expected)
+        {
+            // Act
+            var result = _sut.ToPascalCase(value);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        [InlineData("a", "A")]
+        [InlineData("foo bar", "FOO BAR")]
+        [InlineData("FOO Bar", "FOO BAR")]
+        public void ToUpper(string value, string expected)
+        {
+            // Act
+            var result = _sut.ToUpper(value);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData(" ", "")]
+        [InlineData(" foo bar ", "foo bar")]
+        public void Trim(string value, string expected)
+        {
+            // Act
+            var result = _sut.Trim(value);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData(" ", "")]
+        [InlineData("foo bar ", "foo bar")]
+        public void TrimEnd(string value, string expected)
+        {
+            // Act
+            var result = _sut.TrimEnd(value);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData(" ", "")]
+        [InlineData(" foo bar ", "foo bar ")]
+        public void TrimStart(string value, string expected)
+        {
+            // Act
+            var result = _sut.TrimStart(value);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("", 0, "")]
+        [InlineData("a", 1, "a")]
+        [InlineData("a", 2, "a")]
+        [InlineData("foo bar", 3, "foo")]
+        public void Truncate(string value, int length, string expected)
+        {
+            // Act
+            var result = _sut.Truncate(value, length);
 
             // Assert
             result.Should().Be(expected);
