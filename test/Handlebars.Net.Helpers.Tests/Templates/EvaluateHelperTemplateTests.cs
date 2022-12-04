4a8e909c628f14c56d42b29b78073de2fe751bbd
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-12-04 12:47:06 +0100

    n

--- test/Handlebars.Net.Helpers.Tests/Templates/EvaluateHelperTemplateTests.cs
+++ test/Handlebars.Net.Helpers.Tests/Templates/EvaluateHelperTemplateTests.cs
@@ -2,50 +2,49 @@ using FluentAssertions;
 using HandlebarsDotNet.Helpers.Extensions;
 using Xunit;
 
-namespace HandlebarsDotNet.Helpers.Tests.Templates
+namespace HandlebarsDotNet.Helpers.Tests.Templates;
+
+public class EvaluateHelperTemplateTests
 {
-    public class EvaluateHelperTemplateTests
+    private readonly IHandlebars _handlebarsContext;
+
+    public EvaluateHelperTemplateTests()
     {
-        private readonly IHandlebars _handlebarsContext;
+        _handlebarsContext = Handlebars.Create();
+        HandlebarsHelpers.Register(_handlebarsContext);
+    }
 
-        public EvaluateHelperTemplateTests()
-        {
-            _handlebarsContext = Handlebars.Create();
-            HandlebarsHelpers.Register(_handlebarsContext);
-        }
+    [Theory]
+    [InlineData(@"{{x}}", 'c')]
+    [InlineData(@"{{x}}", "s")]
+    [InlineData(@"{{x}}", 1)]
+    [InlineData(@"{{x}}", int.MaxValue)]
+    [InlineData(@"{{x}}", long.MaxValue)]
+    [InlineData(@"{{x}}", float.MaxValue)]
+    [InlineData(@"{{x}}", double.MaxValue)]
+    public void Evaluate(string template, object x)
+    {
+        // Arrange
+        var data = new { x };
 
-        [Theory]
-        [InlineData(@"{{x}}", 'c')]
-        [InlineData(@"{{x}}", "s")]
-        [InlineData(@"{{x}}", 1)]
-        [InlineData(@"{{x}}", int.MaxValue)]
-        [InlineData(@"{{x}}", long.MaxValue)]
-        [InlineData(@"{{x}}", float.MaxValue)]
-        [InlineData(@"{{x}}", double.MaxValue)]
-        public void Evaluate(string template, object x)
-        {
-            // Arrange
-            var data = new { x };
+        // Act
+        var result = _handlebarsContext.Evaluate(template, data);
 
-            // Act
-            var result = _handlebarsContext.Evaluate(template, data);
+        // Assert
+        result.Should().Be(x);
+    }
 
-            // Assert
-            result.Should().Be(x);
-        }
+    [Fact]
+    public void EvaluateDecimal()
+    {
+        // Arrange
+        var x = decimal.MaxValue;
+        var data = new { x };
 
-        [Fact]
-        public void EvaluateDecimal()
-        {
-            // Arrange
-            var x = decimal.MaxValue;
-            var data = new { x };
+        // Act
+        var result = _handlebarsContext.Evaluate(@"{{x}}", data);
 
-            // Act
-            var result = _handlebarsContext.Evaluate(@"{{x}}", data);
-
-            // Assert
-            result.Should().Be(x);
-        }
+        // Assert
+        result.Should().Be(x);
     }
 }
