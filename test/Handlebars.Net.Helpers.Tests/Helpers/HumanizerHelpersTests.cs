8a9c4a174001ba51d5bc11c3fe288b6ae17f0dda
홍정화 <jhhong@miksystem.com>
2022-12-19 16:46:52 +0900

    revert the changes to the unit-tests

--- test/Handlebars.Net.Helpers.Tests/Helpers/HumanizerHelpersTests.cs
+++ test/Handlebars.Net.Helpers.Tests/Helpers/HumanizerHelpersTests.cs
@@ -335,14 +335,14 @@ namespace HandlebarsDotNet.Helpers.Tests.Helpers
         }
 
         [Theory]
-        [InlineData(10, null, null, null, "Long text?")]
-        [InlineData(10, null, "FixedLength", null, "Long text?")]
+        [InlineData(10, null, null, null, "Long text…")]
+        [InlineData(10, null, "FixedLength", null, "Long text…")]
         [InlineData(10, "---", "FixedLength", null, "Long te---")]
-        [InlineData(6, null, "FixedNumberOfCharacters", null, "Long t?")]
+        [InlineData(6, null, "FixedNumberOfCharacters", null, "Long t…")]
         [InlineData(6, "---", "FixedNumberOfCharacters", null, "Lon---")]
-        [InlineData(2, null, "FixedNumberOfWords", null, "Long text?")]
+        [InlineData(2, null, "FixedNumberOfWords", null, "Long text…")]
         [InlineData(2, "---", "FixedNumberOfWords", null, "Long text---")]
-        [InlineData(10, null, "FixedLength", "Left", "?truncate")]
+        [InlineData(10, null, "FixedLength", "Left", "… truncate")]
         public void Truncate(int length, string? separator, string? truncator, string? truncateFrom, string expected)
         {
             // Arrange
