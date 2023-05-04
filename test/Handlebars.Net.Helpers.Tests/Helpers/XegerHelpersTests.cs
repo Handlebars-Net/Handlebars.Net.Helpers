f7f21b109e598d6b80d4ee5bf9c02981401f071b
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-05-04 12:32:30 +0200

    .

--- test/Handlebars.Net.Helpers.Tests/Helpers/XegerHelpersTests.cs
+++ test/Handlebars.Net.Helpers.Tests/Helpers/XegerHelpersTests.cs
@@ -2,30 +2,39 @@
 using Moq;
 using Xunit;
 
-namespace HandlebarsDotNet.Helpers.Tests.Helpers
+namespace HandlebarsDotNet.Helpers.Tests.Helpers;
+
+public class XegerHelpersTests
 {
-    public class XegerHelpersTests
+    private readonly Mock<IHandlebars> _contextMock;
+
+    private readonly XegerHelpers _sut;
+
+    public XegerHelpersTests()
     {
-        private readonly Mock<IHandlebars> _contextMock;
+        _contextMock = new Mock<IHandlebars>();
+        _contextMock.SetupGet(c => c.Configuration).Returns(new HandlebarsConfiguration());
 
-        private readonly XegerHelpers _sut;
+        _sut = new XegerHelpers(_contextMock.Object);
+    }
 
-        public XegerHelpersTests()
-        {
-            _contextMock = new Mock<IHandlebars>();
-            _contextMock.SetupGet(c => c.Configuration).Returns(new HandlebarsConfiguration());
+    [Fact]
+    public void Xeger_Generate_Test1()
+    {
+        // Act
+        var result = _sut.Generate("[1-9]{1}\\d{3}");
 
-            _sut = new XegerHelpers(_contextMock.Object);
-        }
+        // Assert
+        int.Parse(result).Should().BeInRange(1000, 9999);
+    }
 
-        [Fact]
-        public void Xeger()
-        {
-            // Act
-            var result = _sut.Generate("[1-9]{1}\\d{3}");
+    [Fact]
+    public void Xeger_Generate_Test2()
+    {
+        // Act
+        var result = _sut.Generate("{[\"]A[\"]:[\"]A[0-9]{3}[1-9][\"]}");
 
-            // Assert
-            int.Parse(result).Should().BeInRange(1000, 9999);
-        }
+        // Assert
+        result.Should().StartWith("{").And.EndWith("}").And.Contain(":");
     }
 }
