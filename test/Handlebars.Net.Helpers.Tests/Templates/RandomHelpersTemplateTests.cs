e1ed69168cf28365449cd006425efeacd4a4d934
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-12-08 20:29:17 +0100

    RandomHelpersTemplateTests.Random_Long2

--- test/Handlebars.Net.Helpers.Tests/Templates/RandomHelpersTemplateTests.cs
+++ test/Handlebars.Net.Helpers.Tests/Templates/RandomHelpersTemplateTests.cs
@@ -29,10 +29,23 @@ public class RandomHelpersTemplateTests
     }
 
     [Fact]
-    public void Random_Long()
+    public void Random_Long1()
     {
         // Arrange
         var action = _handlebarsContext.Compile("{{Random.Generate Type=\"Long\" Min=1000000000 Max=9999999999}}");
+
+        // Act
+        var result = action("");
+
+        // Assert
+        long.Parse(result).Should().BeInRange(1000000000, 9999999999);
+    }
+
+    [Fact]
+    public void Random_Long2()
+    {
+        // Arrange
+        var action = _handlebarsContext.Compile("{{#Random Type=\"Long\" Min=1000000000 Max=9999999999}}{{this}}{{/Random}}");
 
         // Act
         var result = action("");
