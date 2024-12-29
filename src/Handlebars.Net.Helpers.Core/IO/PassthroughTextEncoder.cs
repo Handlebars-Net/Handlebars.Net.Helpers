9da73355354cf502a8a277e4b0e3fe10d43a791a
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-12-29 09:10:27 +0100

    PassthroughTextEncoder

---
+++ src/Handlebars.Net.Helpers.Core/IO/PassthroughTextEncoder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace HandlebarsDotNet.Helpers.IO;
+
+/// <summary>
+/// A text encoder that implements <see cref="ITextEncoder"/> and directly passes through the input text to the target without any encoding.
+/// </summary>
+public class PassthroughTextEncoder : ITextEncoder
+{
+    /// <summary>
+    /// Writes the given StringBuilder text directly to the target TextWriter.
+    /// </summary>
+    /// <param name="text">The StringBuilder containing the text.</param>
+    /// <param name="target">The TextWriter to write the text to.</param>
+    public void Encode(StringBuilder text, TextWriter target)
+    {
+        target.Write(text);
+    }
+
+    /// <summary>
+    /// Writes the given string text directly to the target TextWriter.
+    /// </summary>
+    /// <param name="text">The string containing the text.</param>
+    /// <param name="target">The TextWriter to write the text to.</param>
+    public void Encode(string text, TextWriter target)
+    {
+        target.Write(text);
+    }
+
+    /// <summary>
+    /// Writes the given text enumerator directly to the target TextWriter.
+    /// </summary>
+    /// <typeparam name="T">The type of the text enumerator.</typeparam>
+    /// <param name="text">The enumerator containing the text.</param>
+    /// <param name="target">The TextWriter to write the text to.</param>
+    public void Encode<T>(T text, TextWriter target) where T : IEnumerator<char>
+    {
+        target.Write(text);
+    }
+}
---
+++ test/Handlebars.Net.Helpers.Tests/IO/PassthroughTextEncoderTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using FluentAssertions;
+using HandlebarsDotNet.Helpers.IO;
+using Xunit;
+
+namespace HandlebarsDotNet.Helpers.Tests.IO;
+
+public class PassthroughTextEncoderTests
+{
+    [Theory]
+    [InlineData("Hello & World", "test Hello & World abc")]
+    [InlineData("1 < 2 > 0", "test 1 < 2 > 0 abc")]
+    [InlineData("Use \"quotes\"", "test Use \"quotes\" abc")]
+    [InlineData("'single quotes'", "test 'single quotes' abc")]
+    [InlineData("あ", "test あ abc")]
+    [InlineData("A", "test A abc")]
+    [InlineData(null, "test  abc")]
+    [InlineData("", "test  abc")]
+    public void UsePassthroughTextEncoder(string value, string expected)
+    {
+        var context = Handlebars.Create();
+        context.Configuration.FormatProvider = CultureInfo.InvariantCulture;
+        context.Configuration.TextEncoder = new PassthroughTextEncoder();
+
+        var model = new
+        {
+            x = value
+        };
+        var action = context.Compile("test {{x}} abc");
+
+        // Act
+        var result = action(model);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
