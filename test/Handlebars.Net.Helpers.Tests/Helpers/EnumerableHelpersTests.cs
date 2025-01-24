5fdc6d2ed88421cb7c77dfc9da2b878e58a6ae29
epDugas <ericpdugas@gmail.com>
2025-01-24 15:33:59 -0600

    Added additional test for Enumerable.Count helper to satisfy test code coverage.

--- test/Handlebars.Net.Helpers.Tests/Helpers/EnumerableHelpersTests.cs
+++ test/Handlebars.Net.Helpers.Tests/Helpers/EnumerableHelpersTests.cs
@@ -66,12 +66,22 @@ namespace HandlebarsDotNet.Helpers.Tests.Helpers
 
         [Fact]
         public void Count()
-        {
+        {   
             // Act
             var result = _sut.Count(_array);
 
             // Assert
             result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Count_NullEnumerable_Zero()
+        {
+            // Act
+            var result = _sut.Count(null);
+
+            // Assert
+            result.Should().Be(0);
         }
 
         [Fact]
